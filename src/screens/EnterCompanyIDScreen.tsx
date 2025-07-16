d79eb9ea406961aa1059fb9d6919abbe67b529af
HasanDroid18 <tahahasan046@gmail.com>
2025-07-16 16:35:09 +0300

    fixed navigation

--- src/screens/EnterCompanyIDScreen.tsx
+++ src/screens/EnterCompanyIDScreen.tsx
@@ -1,14 +1,27 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useNavigationState } from '@react-navigation/native';
 import Container from '../components/Container';
 
 const EnterCompanyIDScreen = () => {
   const navigation = useNavigation();
 
+  const routes = useNavigationState(state => state.routes);
+  const isFromSettingsStack = routes.some(route =>
+    ['Settings', 'SetCompanyID'].includes(route.name),
+  );
+
   const continueBtn = () => {
-    //@ts-ignore
-    navigation.navigate('PickVoice');
+   if (isFromSettingsStack) {
+      navigation.reset({
+        index: 0,
+        //@ts-ignore
+        routes: [{ name: 'Settings' }],
+      });
+    }else {
+      //@ts-ignore
+      navigation.navigate('PickVoice');
+    }
   };
   return (
     <View className="flex-1 items-center justify-center bg-white">
